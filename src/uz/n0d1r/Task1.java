820c563868fb173fbc09265acff4cf3ae99d47e3
N0D1R <n.sadullayev@gmail.com>
2019-09-23 15:43:03 +0500

    task1 plus function implemented

---
+++ src/uz/n0d1r/Task1.java
@@ -0,0 +1,20 @@
+package uz.n0d1r;
+
+/**
+ * Created by N0D1R on 23-Sep-19.
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 3;
+        System.out.println(plus(a,b));
+
+    }
+
+    private static int plus(int a, int b) {
+        return a+b;
+    }
+
+
+}
